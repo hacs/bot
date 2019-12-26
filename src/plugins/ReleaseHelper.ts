42753dc10e03468b41ef5577302beb59f9feab8c
ludeeus <joasoe@gmail.com>
2019-12-26 17:46:09 +0000

    Minor updates

--- src/plugins/ReleaseHelper.ts
+++ src/plugins/ReleaseHelper.ts
@@ -1,14 +1,28 @@
-import { Application } from 'probot'
-import { ExecutionFilter } from './ExecutionFilter'
-import { IsAdmin } from './ExecutionFilter'
+import { Application } from "probot";
+import { ExecutionFilter } from "./ExecutionFilter";
+import { IsAdmin } from "./ExecutionFilter";
 
 export const ReleaseHelper = (app: Application) => {
   app.on("issue_comment.created", async context => {
     if (!ExecutionFilter(context)) return;
+    if (!context.payload.comment.body.startsWith("@hacs-bot ")) return;
 
-    console.log(context.payload.comment)
-    console.log(context.payload.sender)
+    const commentid: number = context.payload.comment.id;
+    const command: string = context.payload.comment.body
+      .split("@hacs-bot ")[0]
+      .toLowerCase();
 
-    console.log("is admin?", IsAdmin(context))
+    if (!IsAdmin(context)) {
+      context.github.reactions.createForIssueComment(
+        context.issue({ comment_id: commentid, content: "confused" })
+      );
+    }
+
+    if (["no", "close"].includes(command)) {
+      context.github.reactions.createForIssueComment(
+        context.issue({ comment_id: commentid, content: "+1" })
+      );
+      context.github.issues.update(context.issue({ state: "closed" }));
+    }
   });
-}
+};
