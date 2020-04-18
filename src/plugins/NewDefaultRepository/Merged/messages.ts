947cf246f418a0b9a2f1925e00d5edf155a69d6f
ludeeus <joasoe@gmail.com>
2020-04-18 10:34:49 +0000

    Adds postToPullRequest

---
+++ src/plugins/NewDefaultRepository/Merged/messages.ts
@@ -0,0 +1,23 @@
+export const messageCommon = `
+Your repository is now added to HACS :tada:
+
+Here is a few resources that can be useful:
+
+- [HACS Discord][hacsdiscord] If you have questions about HACS this is the best place for it.
+- [Home Assistant Developer Blog][hadevblog] Make sure you stay up to date.
+- [Home Assistant Devs @ twitter][hadevtwitter]
+- [HACS @ twitter][hacstwitter]
+`;
+
+export const messageIntegrations = `
+- [Validate your integration with this GitHub Action][hassfestaction]
+`;
+
+export const messageLinks = `
+<!-- Links -->
+[hacsdiscord]: https://discord.gg/apgchf8
+[hacstwitter]: https://twitter.com/HACSIntegration
+[hadevblog]: https://developers.home-assistant.io/blog
+[hadevtwitter]: https://twitter.com/hass_devs
+[hassfestaction]: https://github.com/home-assistant/actions#hassfest
+`;
