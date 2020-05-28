7baba4cbfb503416e4bd74c00aca9acf17fd925e
ludeeus <joasoe@gmail.com>
2020-05-28 15:08:48 +0000

    Actually enabled commands

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,7 @@ import { Application } from "probot";
 import { initDebug } from "./plugins/Debug/plugin";
 import { initKnownIssues } from "./plugins/KnownIssues/plugin";
 import { initGreeter } from "./plugins/Greeter/plugin";
+import { initCommands } from "./plugins/Commands/plugin";
 import { initHacktoberfest } from "./plugins/Hacktoberfest/plugin";
 import { initClearTempLabels } from "./plugins/ClearTempLabels/plugin";
 import { initNewDefaultRepository } from "./plugins/NewDefaultRepository/plugin";
@@ -13,7 +14,7 @@ export = (app: Application) => {
   initKnownIssues(app);
   initGreeter(app);
   initHacktoberfest(app);
-
+  initCommands(app);
   initClearTempLabels(app);
   initNewDefaultRepository(app);
   initReleaseHelper(app);
