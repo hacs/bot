5f1deb16b0e9cd1e2018a95988a5285f055510cf
ludeeus <joasoe@gmail.com>
2020-05-29 17:11:58 +0000

    Add known issue NotSupportedError

--- src/plugins/KnownIssues/list.ts
+++ src/plugins/KnownIssues/list.ts
@@ -17,4 +17,11 @@ There is nothing that can be done from HACS, since this happens _before_ HACS is
 If you run docker, do not use a user/group for the container (stick to root).
 If you have a \`deps\` dir in your config dir, delete it.`,
   },
+  {
+    search: [
+      `Uncaught NotSupportedError: Failed to execute 'define' on 'CustomElementRegistry': the name "hacs-frontendbase" has already been used with this registry`,
+    ],
+    lables: ["Issue not in HACS"],
+    message: `You get (Uncaught NotSupportedError: Failed to execute 'define' on 'CustomElementRegistry': the name "hacs-frontendbase" has already been used with this registry) in your logs every time you upgrade HACS, this **is** expected, to solve it clear your browser cache.`,
+  },
 ];
