822380d5bd4b0a912fd3a9370fd42c15493436cc
Ludeeus <ludeeus@ludeeus.dev>
2020-10-22 20:41:17 +0000

    fix issue

--- src/util/extractLabels.ts
+++ src/util/extractLabels.ts
@@ -5,5 +5,5 @@ export const NAME = "extractLabels";
 
 export function extractLabels(context: Context) {
   const PRorISSUE = context.payload.issue || context.payload.pull_request;
-  return (PRorISSUE.labels as WebhookPayloadIssuesIssue["labels"]) || [];
+  return (PRorISSUE?.labels as WebhookPayloadIssuesIssue["labels"]) || [];
 }
