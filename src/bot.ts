60cda5bb04f762062015b06be18b9163784501b4
Ludeeus <ludeeus@ludeeus.dev>
2020-10-22 20:38:36 +0000

    remove hacktoberfest

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,6 @@ import { initDebug } from "./plugins/Debug/plugin";
 import { initKnownIssues } from "./plugins/KnownIssues/plugin";
 import { initGreeter } from "./plugins/Greeter/plugin";
 import { initCommands } from "./plugins/Commands/plugin";
-import { initHacktoberfest } from "./plugins/Hacktoberfest/plugin";
 import { initClearTempLabels } from "./plugins/ClearTempLabels/plugin";
 import { initNewDefaultRepository } from "./plugins/NewDefaultRepository/plugin";
 import { initReleaseHelper } from "./plugins/ReleaseHelper/plugin";
@@ -15,7 +14,6 @@ export = (app: Application) => {
   initBlankIssues(app);
   initKnownIssues(app);
   initGreeter(app);
-  //initHacktoberfest(app);
   initCommands(app);
   initClearTempLabels(app);
   initNewDefaultRepository(app);
