6105e4ce4c7c3cb551c51762fe193d14f5baea3d
Ludeeus <ludeeus@ludeeus.dev>
2022-02-16 15:59:22 +0000

    fix getter

--- src/utils/nextMilestone.ts
+++ src/utils/nextMilestone.ts
@@ -3,7 +3,7 @@ import { App } from 'octokit'
 export const getNextMilestone = async (app: App) => {
   const { data: milestones } = await app.octokit.rest.issues.listMilestones({
     owner: 'hacs',
-    repo: 'payload',
+    repo: 'integration',
   })
 
   return milestones.find((milestone) => milestone.title === 'next')
