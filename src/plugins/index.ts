44837cbe537179c6d88a4f30368fb1ae2090101d
ludeeus <joasoe@gmail.com>
2024-07-10 18:08:03 +0000

    convert greeter

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,10 +3,12 @@ import type { IssuePullPayload } from '../types'
 import DebugPlugin from './debug'
 import IssuesLabeledDuplicate from './issues.labeled.duplicate'
 import IssuesLabeledInvalid from './issues.labeled.invalid'
+import IssuesOpenedGreeter from './issues.opened.greeter'
 
 type Plugin = (bot: GitHubBot, payload: IssuePullPayload) => Promise<void>
 
 export const plugins: Record<string, Plugin[]> = {
   base: [DebugPlugin],
   'issues.labeled': [IssuesLabeledDuplicate, IssuesLabeledInvalid],
+  'issues.opened': [IssuesOpenedGreeter],
 }
--- src/plugins/issues.opened.greeter.ts
+++ src/plugins/issues.opened.greeter.ts
@@ -1,39 +1,40 @@
-import { App } from 'octokit'
-import { IssuePayload } from '../types'
-import { extractLabels } from '../utils/extractLabels'
+import { IssuePullPayload, PayloadIsIssue } from '../types'
+
+import { GitHubBot } from '../github.bot'
 import { extractOwnerRepo } from '../utils/extractOwnerRepo'
 import { senderIsBot } from '../utils/filter'
 
-export default async (app: App, payload: IssuePayload): Promise<void> => {
-  if (senderIsBot(payload)) return
+export default async (
+  bot: GitHubBot,
+  payload: IssuePullPayload,
+): Promise<void> => {
+  if (
+    senderIsBot(payload) ||
+    !PayloadIsIssue(payload) ||
+    payload.action !== 'opened'
+  ) {
+    return
+  }
 
   if (extractOwnerRepo(payload).repo !== 'integration') {
-    await app.octokit.rest.issues.createComment({
+    await bot.github.octokit.rest.issues.createComment({
       ...extractOwnerRepo(payload),
       issue_number: payload.issue.number,
       body: `Issues should be opened in [hacs/integration](https://github.com/hacs/integration/issues) see [docs](https://hacs.xyz/docs/issues) for more info.`,
     })
-    await app.octokit.rest.issues.update({
+    await bot.github.octokit.rest.issues.update({
       ...extractOwnerRepo(payload),
       issue_number: payload.issue.number,
       state: 'closed',
     })
-    await app.octokit.rest.issues.lock({
+    await bot.github.octokit.rest.issues.lock({
       ...extractOwnerRepo(payload),
       issue_number: payload.issue.number,
     })
     return
   }
 
-  const isInvalid: boolean = extractLabels(payload)
-    .map((label) => label.name)
-    .includes('invalid' || 'Issue not in HACS')
-
-  if (isInvalid) {
-    return
-  }
-
-  await app.octokit.rest.issues.createComment({
+  await bot.github.octokit.rest.issues.createComment({
     ...extractOwnerRepo(payload),
     issue_number: payload.issue.number,
     body: `Make sure you have read the [issue guidelines](https://hacs.xyz/docs/issues) and that you filled out the **entire** template.\n
