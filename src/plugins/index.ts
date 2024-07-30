33e3c6de9e17a36f18da4e09964039bb062b9a8f
ludeeus <joasoe@gmail.com>
2024-07-30 11:05:11 +0000

    Add repository metrics

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,6 @@
 import type { GitHubBot } from '../github.bot'
 import BaseDebug from './base.debug'
+import BaseMetrics from './base.metrics'
 import IssuesLabeledDuplicate from './issues.labeled.duplicate'
 import IssuesLabeledInvalid from './issues.labeled.invalid'
 import IssuesOpenedGreeter from './issues.opened.greeter'
@@ -9,7 +10,7 @@ import WorkflowRunCompeted from './workflow_run.completed'
 type Plugin = (bot: GitHubBot, payload: any) => Promise<void>
 
 export const plugins: Record<string, Plugin[]> = {
-  base: [BaseDebug],
+  base: [BaseDebug, BaseMetrics],
   'issues.labeled': [IssuesLabeledDuplicate, IssuesLabeledInvalid],
   'issues.opened': [IssuesOpenedGreeter],
   'workflow_run.completed': [WorkflowRunCompeted],
