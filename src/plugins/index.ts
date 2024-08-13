045d4eba92314f2bc2e1c56d034d8a24bb72a7ac
ludeeus <joasoe@gmail.com>
2024-08-13 12:18:19 +0000

    disable debug

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,4 @@
 import type { GitHubBot } from '../github.bot'
-import BaseDebug from './base.debug'
 import BaseMetrics from './base.metrics'
 import IssuesLabeledDuplicate from './issues.labeled.duplicate'
 import IssuesLabeledInvalid from './issues.labeled.invalid'
@@ -10,7 +9,7 @@ import WorkflowRunCompeted from './workflow_run.completed'
 type Plugin = (bot: GitHubBot, payload: any) => Promise<void>
 
 export const plugins: Record<string, Plugin[]> = {
-  base: [BaseDebug, BaseMetrics],
+  base: [BaseMetrics],
   'issues.labeled': [IssuesLabeledDuplicate, IssuesLabeledInvalid],
   'issues.opened': [IssuesOpenedGreeter],
   'workflow_run.completed': [WorkflowRunCompeted],
